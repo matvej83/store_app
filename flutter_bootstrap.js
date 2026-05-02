c7b38cf313a2f2b27f93febb4fd0e15eda371746
matvej83 <55102679+matvej83@users.noreply.github.com>
2026-05-02 16:30:58 +0000

    Deploying to gh-pages from @ matvej83/clean_architecture_test@926d23c29b01c5bd07beecf73055a3854afe02cf 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1870188046" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "298535939" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
